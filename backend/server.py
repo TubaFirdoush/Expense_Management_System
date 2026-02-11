4b8176ffb14e87e7ddac0209691a62a831a92498
TubaFirdoush <154806591+TubaFirdoush@users.noreply.github.com>
2026-02-11 11:39:29 +0530

    Add expense management endpoints in server.py
    Implement expense management API with FastAPI

---
+++ backend/server.py
@@ -0,0 +1,59 @@
+from turtledemo.penrose import start
+
+from fastapi import FastAPI, HTTPException
+from datetime import date
+from pydantic import BaseModel
+import db_helper
+from typing import List
+
+from logging_setup import setup_logger
+app = FastAPI()
+
+class Expense(BaseModel):
+    # expense_date: date
+    amount: float
+    category: str
+    notes: str
+
+class DateRange(BaseModel):
+    # expense_date: date
+    start_date: date
+    end_date: date
+
+
+loger = setup_logger('server')
+
+
+@app.get("/expenses/{expense_date}", response_model=List[Expense])
+def get_expenses(expense_date: date):
+    expenses = db_helper.fetch_expenses_for_date(expense_date)
+    if expenses is None:
+        raise HTTPException(status_code=500, detail="Failed to fetch expense database")
+    return expenses
+
+@app.post("/expenses/{expense_date}")
+def add_or_update_expense(expense_date: date, expenses:List[Expense]):
+    db_helper.delete_expenses_for_date(expense_date)
+    for expense in expenses:
+        db_helper.insert_expense(expense_date, expense.amount, expense.category, expense.notes)
+
+    return {"message": "Successfully updated expense"}
+
+@app.post("/analytics/")
+def get_analytics(date_range: DateRange):
+    data = db_helper.fetch_expense_summary(date_range.start_date, date_range.end_date)
+    if data is None:
+        raise HTTPException(status_code=500, detail="Failed to fetch expense summary from database")
+    total = sum([row['total'] for row in data])
+
+    breakdown = {}
+
+
+    for row in data:
+        percentage = (row['total'] / total) * 100 if total !=0 else 0
+        breakdown[row['category']] = {
+            "total": row['total'],
+            "percentage": percentage,
+        }
+
+    return breakdown
